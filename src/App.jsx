0cded5a3a65b9e2c895aec771a5c9e8356200313
unknown <santimallalakshmidhar@gmail.com>
2025-03-28 13:52:21 +0530

    sfsdsg

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,50 @@
+import { useEffect, useState } from "react";
 import UpdateItem from "./components/UpdateItem";
-
+import axios from "axios";
+import { Routes,Route, useNavigate } from "react-router-dom";
 // use the following link to get the data
 // `/doors` will give you all the doors, to get a specific door use `/doors/1`.
-const API_URI = `http://${import.meta.env.VITE_API_URI}/doors`;
+const API_URI = `http://localhost:8000/doors`;
 
 function App() {
   // Get the existing item from the server
-  // const [item, setItem] = useState(null);
+   const [item, setItem] = useState([])
   // pass the item to UpdateItem as a prop
+  const navigate=useNavigate();
+useEffect(()=>{
+const ab = async()=>{
+  try{
+ const {data}=await axios.get(API_URI)
+ setItem(data)
+ 
+ console.log(data)
+  }
+  catch(e){
+    console.log(e)
+  }
+}
+ab();
+},[item])
+  return (
+    <>
+    <div>
+    {
+      item.map(item=>(
+        <div key={item.id}>
+          <h5>{item.name}</h5>
+          <h5>{item.status}</h5>
+          <button onClick={()=>navigate(`/edit/${item.id}`)} >update</button>
+        </div>
+      ))
+    }
+   </div>
+    <Routes>
 
-  return <UpdateItem />;
+      <Route path='/edit/:id' element={<UpdateItem/>}/>
+    </Routes>
+   
+    </>
+  )
 }
 
 export default App;
--- src/components/UpdateItem.jsx
+++ src/components/UpdateItem.jsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 const UpdateItem = ({ item }) => {
     // 1. Create a state for the form
     // 2. Create a function to handle the form submission
     // 3. Create a function to handle the form input changes
+    const [name,SetName] = useState("")
+    const [status,setStatus] = useState("")
+const {id}=useParams()
+console.log(id)
+    const handleSubmit = async(e)=>{
+        e.preventDefault()
+        const {data}=await axios.put(`http://localhost:8000/doors/${id}`,{name,status})
+        console.log(data)
+    }
+
+
 
     // your code here
-    return null;
+    return (
+        <>
+        <form onSubmit={handleSubmit}>
+            <p>name<input value={name} onChange={(e)=>SetName(e.target.value)} type="text"></input></p>
+            <p>status<select onChange={(e)=>setStatus(e.target.value)} value={status}>
+                <option value="open">open</option>
+                 <option value="close" >closed</option>   
+                    </select></p>
+            <button type="submit">submit</button>        
+        </form>
+        
+        
+        
+        </>
+    )
 };
 
 export default UpdateItem;
